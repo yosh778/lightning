d91db155bb5dc22f122d84ed838b6f85d3ba47ee
venger_74@hotmail.fr <venger_74@hotmail.fr@4aba0ea6-2657-9f67-e26c-578093356fdb>
2011-04-28 13:41:36 +0000

    added menu files

---
+++ menu.h
@@ -0,0 +1,15 @@
+/*
+menu.h
+by Yosh alias Hitman_07
+
+28/04/11
+
+*/
+
+
+# ifndef H_MENU
+# define H_MENU
+
+int menu(OSL_COLOR *bgstartColor, OSL_IMAGE *bgstart, OSL_IMAGE *start, int *alpha);
+
+# endif
