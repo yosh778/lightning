c00d64c2c8019d5275d7fe583d0927de215f7a99
venger_74@hotmail.fr <venger_74@hotmail.fr@4aba0ea6-2657-9f67-e26c-578093356fdb>
2011-04-28 17:24:45 +0000

    Menu implementation

--- menu.h
+++ menu.h
@@ -10,6 +10,6 @@ by Yosh alias Hitman_07
 # ifndef H_MENU
 # define H_MENU
 
-int menu(OSL_COLOR *bgstartColor, OSL_IMAGE *bgstart, OSL_IMAGE *start, int *alpha);
+int menu(OSL_COLOR *bgstartColor, OSL_IMAGE *bgstart, OSL_IMAGE *start, int *alpha, int* difficulty);
 
 # endif
