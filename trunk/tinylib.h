759f44ceacef722fbe46fe3d6e3826c8bf21deb5
venger_74@hotmail.fr <venger_74@hotmail.fr@4aba0ea6-2657-9f67-e26c-578093356fdb>
2011-05-03 10:07:52 +0000

    Added sound fx

--- trunk/tinylib.h
+++ trunk/tinylib.h
@@ -15,8 +15,9 @@ bool splashScreen(char *location, int timeLimit, int decay, OSL_COLOR bgColor);
 bool startScreen(OSL_FONT *f, int level, bool sublevel);
 bool overScreen(OSL_FONT *f, bool over);
 bool quitScreen(OSL_FONT *f);
-void settings(int *bg_col_m, Color *Bg_col, OSL_FONT *f, OSL_COLOR *bgstartColor, int *difficulty);
+void settings(OSL_SOUND *cancel, OSL_SOUND *mstart, bool *returned, int *bg_col_m, Color *Bg_col, OSL_FONT *f, OSL_COLOR *bgstartColor, int *difficulty);
 int load(Color *Bg_col, int *difficulty);
 int save(Color *Bg_col, int *difficulty);
+bool strtsnd_if(OSL_SOUND *cancel, OSL_SOUND *mstart, bool *returned);
 
 # endif
