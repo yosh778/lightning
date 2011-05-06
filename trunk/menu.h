0c74cf37f077fbe025e126475d4a7606de796da0
venger_74@hotmail.fr <venger_74@hotmail.fr@4aba0ea6-2657-9f67-e26c-578093356fdb>
2011-05-06 15:28:01 +0000

    Menu fx added, attempted to fix bug on quit menu

--- trunk/menu.h
+++ trunk/menu.h
@@ -10,6 +10,6 @@ by Yosh alias Hitman_07
 # ifndef H_MENU
 # define H_MENU
 
-int menu(OSL_SOUND *cancel, OSL_SOUND *mstart, bool *returned, int *bg_col_m, Color *Bg_col, OSL_FONT *f, OSL_COLOR *bgstartColor, OSL_IMAGE *bgstart, OSL_IMAGE *start, int *alpha, int* difficulty);
+int menu(OSL_SOUND *fx_up, OSL_SOUND *fx_dn, OSL_SOUND *cancel, OSL_SOUND *mstart, bool *returned, int *bg_col_m, Color *Bg_col, OSL_FONT *f, OSL_COLOR *bgstartColor, OSL_IMAGE *bgstart, OSL_IMAGE *start, int *alpha, int* difficulty);
 
 # endif
